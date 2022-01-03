5aa0a14da0edf46ba1c9c66bdf9b3a679e379703
kenjiuno <ku@digitaldolphins.jp>
2022-01-04 00:59:30 +0900

    WIP

--- net/ipv6/ipip6gw/files/ipip6gw.sh
+++ net/ipv6/ipip6gw/files/ipip6gw.sh
@@ -90,7 +90,9 @@ proto_ipip6gw_setup() {
 	proto_send_update "$cfg" || {
 		logger -t "ipip6gw" -p warn \
 			It seems to fail on creation of ipip6 tunnel interface. \
-			Please observe output of ip-route (`ip -4 r` and `ip -6 r`) and ip-tunnel (`ip -4 t` and `ip -6 t`). \
+			Please observe output of \
+			ip-route (\`ip -4 r\` and \`ip -6 r\`) and \
+			ip-tunnel (\`ip -4 t\` and \`ip -6 t\`). \
 			Ensure there is no duplication of local/remote IPv6 addresses for this tunnel interface. \
 			Perhaps it may need to purge their routes/tunnels before starting tunnel interface.
 
