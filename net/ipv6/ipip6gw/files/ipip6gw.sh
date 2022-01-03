fd567ec9d8894241da62a91974610f8ac520c535
kenjiuno <ku@digitaldolphins.jp>
2022-01-04 01:05:15 +0900

    WIP

--- net/ipv6/ipip6gw/files/ipip6gw.sh
+++ net/ipv6/ipip6gw/files/ipip6gw.sh
@@ -88,14 +88,14 @@ proto_ipip6gw_setup() {
 	proto_close_data
 
 	proto_send_update "$cfg" || {
-		logger -t "ipip6gw" -p warn \
-			It seems to fail on creation of ipip6 tunnel interface. \
-			Please observe output of \
-			ip-route \(\`ip -4 r\` and \`ip -6 r\`\) and \
-			ip-tunnel \(\`ip -4 t\` and \`ip -6 t\`\). \
-			Ensure there is no duplication of local/remote IPv6 addresses for this tunnel interface. \
+		logger -t "ipip6gw" -p warn << EOS
+			It seems to fail on creation of ipip6 tunnel interface.
+			Please observe output of
+			ip-route (\`ip -4 r\` and \`ip -6 r\`) and
+			ip-tunnel (\`ip -4 t\` and \`ip -6 t\`).
+			Ensure there is no duplication of local/remote IPv6 addresses for this tunnel interface.
 			Perhaps it may need to purge their routes/tunnels before starting tunnel interface.
-
+EOS
 		# avoid very frequent restart loop.
 		sleep 1
 	}
